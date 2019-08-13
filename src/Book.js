8dcb3c7cc78e74f985322463514d13bf43ce94cb
James Lowry <jl@balkerne.com>
2019-08-13 18:42:10 +0100

    Updated app to use new components, changed book and added test data

--- src/Book.js
+++ src/Book.js
@@ -5,13 +5,15 @@ function Book(props){
     return (
         <div className="book">
         <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${props.backgroundImage})`}}></div>
+          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${props.previewLink})`}}></div>
           <div className="book-shelf-changer">
             <ShelfChangeButton/>
           </div>
         </div>
         <div className="book-title">{props.title}</div>
-        <div className="book-authors">{props.author}</div>
+        <div className="book-authors">{props.authors.map(author => (
+            author
+          ))}</div>
       </div>
     )
 }
