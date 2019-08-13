0657292f9f11e3aef78d6df535e6108c6999b35e
James Lowry <jl@balkerne.com>
2019-08-13 18:40:19 +0100

    Created book case comp

---
+++ src/BookCase.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import BookShelf from './BookShelf';
+
+function BookCase(props){
+  return(
+    <div className="list-books">
+      <div className="list-books-content">
+        <div>
+          {props.shelves.map(shelf => (
+            <BookShelf name={getShelfName(shelf)} books={props.books.filter((book)=>book.shelf===shelf)} />
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+
+}
+
+function getShelfName(shelfName){
+    if(shelfName==='read'){
+      return 'Read'
+    }
+    else if(shelfName==='currentlyReading'){
+      return 'Currently Reading'
+    }
+    else return 'Want to read'
+}
+
+export default BookCase
