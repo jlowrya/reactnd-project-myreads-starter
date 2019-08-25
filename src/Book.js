4aa01417ac418725a1b4b6a1537876a2f3108455
James Lowry <jl@balkerne.com>
2019-08-25 17:37:26 +0100

    Bug fix - books with no cover images no longer crash the app.

--- src/Book.js
+++ src/Book.js
@@ -1,36 +1,35 @@
 import React, {Component} from 'react';
 import ShelfChangeButton from './ShelfChangeButton';
 
+const noImage = {
+  textAlign: 'center',
+  fontWeight: 'bold'
+}
+
 class Book extends Component{
   constructor(props){
     super(props)
-    this.state = {
-      shelf: this.props.book.shelf,
-    }
-
     this.handleChange = this.handleChange.bind(this)
   }
 
   handleChange = (event) => {
     const shelf =  event.target.value
-
-    this.setState({shelf})
     this.props.changeShelf(this.props.book, shelf)
   }
 
    render(){
+     const image = this.props.book.imageLinks ? `url(${this.props.book.imageLinks["smallThumbnail"]})`: undefined
      return (
          <div className="book">
          <div className="book-top">
-           <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.props.book.imageLinks["smallThumbnail"]})`}}></div>
+           <div className="book-cover" style={{display: 'grid', width: 128, height: 193, backgroundImage: image, alignItems: 'center'}}>
+             {image===undefined ? <p style={noImage}>Image unavailable</p>:''}</div>
            <div className="book-shelf-changer">
              <ShelfChangeButton book={this.props.book} handleChange={this.handleChange}/>
            </div>
          </div>
          <div className="book-title">{this.props.book.title}</div>
-         <div className="book-authors">{this.props.book.authors === undefined ? <br/>:this.props.book.authors.map(author => (
-             author
-           ))}</div>
+         <div className="book-authors">{this.props.book.authors === undefined ? <br/>:this.props.book.authors.join(', ')}</div>
        </div>
      )
    }
